6b9c8500826cd72e69f136b8516b2939dfa2063d
Spiros_Vardis <41401981+spirosvar@users.noreply.github.com>
2020-06-12 19:43:47 +0300

    Questionnaire
    Questionnaire

--- Covid/src/Civilian_GUI.java
+++ Covid/src/Civilian_GUI.java
@@ -4,10 +4,14 @@ import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JLabel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JCheckBox;
 
 public class Civilian_GUI extends JFrame {
-	public Civilian_GUI() {
+	public Civilian_GUI(Doctor doc) {
 		getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
 				FormSpecs.RELATED_GAP_COLSPEC,
 				FormSpecs.DEFAULT_COLSPEC,
@@ -41,29 +45,40 @@ public class Civilian_GUI extends JFrame {
 				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,}));
 		
-		JLabel lblNewLabel = new JLabel("Διάλεξε τον προσωπικό σου γιατρό:");
+		JLabel lblNewLabel = new JLabel("Choose your personal Doctor from the list: ");
 		getContentPane().add(lblNewLabel, "14, 2");
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("Γιατρός 1");
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Doctor 1� 1");
 		getContentPane().add(chckbxNewCheckBox, "14, 4");
 		
-		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("Γιατρός 2");
+		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("Doctor 2� 2");
 		getContentPane().add(chckbxNewCheckBox_1, "14, 6");
 		
-		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("Γιατρός 3");
+		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("Doctor 3� 3");
 		getContentPane().add(chckbxNewCheckBox_2, "14, 8");
 		
-		JCheckBox chckbxNewCheckBox_3 = new JCheckBox("Γιατρός 4");
+		JCheckBox chckbxNewCheckBox_3 = new JCheckBox("Doctor 4� 4");
 		getContentPane().add(chckbxNewCheckBox_3, "14, 10");
 		
-		JCheckBox chckbxNewCheckBox_4 = new JCheckBox("Γιατρός 5");
+		JCheckBox chckbxNewCheckBox_4 = new JCheckBox("Doctor 5� 5");
 		getContentPane().add(chckbxNewCheckBox_4, "14, 12");
 		
-		JCheckBox chckbxNewCheckBox_5 = new JCheckBox("Γιατρός 6");
+		JCheckBox chckbxNewCheckBox_5 = new JCheckBox("Doctor 6� 6");
 		getContentPane().add(chckbxNewCheckBox_5, "14, 14");
 		
-		JCheckBox chckbxNewCheckBox_6 = new JCheckBox("Γιατρός 7");
+		JCheckBox chckbxNewCheckBox_6 = new JCheckBox("Doctor 7� 7");
 		getContentPane().add(chckbxNewCheckBox_6, "14, 16");
-	}
+		
+	}}
+		
+		
+	
+			
+					 
+				 
+			 
+		 
 
-}
+
+
---
+++ Covid/src/Questionnaire.java
@@ -0,0 +1,175 @@
+import javax.swing.JFrame;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JCheckBox;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class Questionnaire extends JFrame
+{
+
+	
+	private int nOfSymptoms=0;
+	
+	
+	public Questionnaire() {
+				
+		
+			getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
+						
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+					
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
+			
+		
+		JCheckBox c7 = new JCheckBox("Fever or chills?");
+		getContentPane().add(c7, "12, 2");
+		c7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(c7.isSelected()) {
+					nOfSymptoms++;
+				}
+			}
+		}); 
+		
+		JCheckBox c = new JCheckBox("Cough?");
+		getContentPane().add(c, "12, 4");
+		c.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(c.isSelected()) {
+					nOfSymptoms++;
+				}
+			}
+		}); 
+		
+		JCheckBox c1 = new JCheckBox("Shortness of breath or difficulty breathing?");
+		getContentPane().add(c1, "12, 6");
+		c1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(c1.isSelected()) {
+					nOfSymptoms++;
+				}
+			}
+		}); 
+		
+		JCheckBox c2 = new JCheckBox("Fatigue?");
+		getContentPane().add(c2, "12, 8");
+		c2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(c2.isSelected()) {
+					nOfSymptoms++;
+				}
+			}
+		}); 
+		
+		JCheckBox c3 = new JCheckBox("Muscle or body aches?");
+		getContentPane().add(c3, "12, 10");
+		c3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(c3.isSelected()) {
+					nOfSymptoms++;
+				}
+			}
+		}); 
+		
+		JCheckBox c4 = new JCheckBox("Headache?");
+		getContentPane().add(c4, "12, 12");
+		c4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(c4.isSelected()) {
+					nOfSymptoms++;
+				}
+			}
+		}); 
+		
+		JCheckBox c5 = new JCheckBox("New loss of taste or smell?");
+		getContentPane().add(c5, "12, 14");
+		c5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(c5.isSelected()) {
+					nOfSymptoms++;
+				}
+			}
+		}); 
+		
+		JCheckBox c6 = new JCheckBox("Nausea or vomiting?");
+		getContentPane().add(c6, "12, 16");
+		c6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(c6.isSelected()) {
+					nOfSymptoms++;
+				}
+			}
+		}); 
+		
+		JCheckBox c8 = new JCheckBox("Soreness?");
+		getContentPane().add(c8, "12, 18");
+		c8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(c8.isSelected()) {
+					nOfSymptoms++;
+				}
+			}
+		}); 
+		
+		JButton btn = new JButton("Submit");
+		getContentPane().add(btn, "18, 18");
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent i) {
+				if(i.getSource().equals(btn)){
+					if(nOfSymptoms >= 6) {
+						Civilian.setRiskStatus(nOfSymptoms);
+					}
+				}
+					
+				}
+			}
+		});
+		
+		
+			
+			
+			
+		}
